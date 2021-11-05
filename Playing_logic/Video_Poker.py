aa9a5375e94f229313083adc801d741fd80681fe
Oleg Sedov <specter21rus@gmail.com>
2021-11-05 23:48:45 +0300

    Added check of correctness of bet size and data type, as well as for cards to be changed

--- Playing_logic/Video_Poker.py
+++ Playing_logic/Video_Poker.py
@@ -2,7 +2,6 @@
 # ♠︎ ♣︎ ♥︎ ♦︎
 
 import random
-
 # Создаем колоду карт
 cards_deck = ['A♠', 'A♣', 'A♥', 'A♦', 'K♠', 'K♣', 'K♥', 'K♦', 'Q♠', 'Q♣', 'Q♥', 'Q♦', 'J♠', 'J♣', 'J♥', 'J♦',
               'T♠', 'T♣', 'T♥', 'T♦', '9♠', '9♣', '9♥', '9♦', '8♠', '8♣', '8♥', '8♦', '7♠', '7♣', '7♥', '7♦',
@@ -17,7 +16,6 @@ ranks_dict = {'A': 14, 'a': 1, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9,
 reversed_ranks_dict = {14: 'A', 1: 'a', 13: 'K', 12: 'Q', 11: 'J', 10: 'T', 9: '9', 8: '8',
                        7: '7', 6: '6', 5: '5', 4: '4', 3: '3', 2: '2'}
 points = 1000
-bet = 0
 combination = ''
 exit_request = ''
 
@@ -83,35 +81,62 @@ def count_win():  # расчитываем выйгрыш
 
 
 payout_table = (f'''Таблица выплат:
-                  {combinations[0]} х {payout_rates[0]}
-                  {combinations[1]} х {payout_rates[1]}
-                  {combinations[2]} x {payout_rates[2]}
-                  {combinations[3]} x {payout_rates[3]}
-                  {combinations[4]} x {payout_rates[4]}
-                  {combinations[5]} x {payout_rates[5]}
-                  {combinations[6]} x {payout_rates[6]}
-                  {combinations[7]} x {payout_rates[7]}
-                  {combinations[8]} x {payout_rates[8]}
-                  ''')
+        {combinations[0]} х {payout_rates[0]}
+        {combinations[1]}{(len(combinations[0]) - len(combinations[1])) * ' '} х {payout_rates[1]}
+        {combinations[2]}{(len(combinations[0]) - len(combinations[2])) * ' '} x {payout_rates[2]}
+        {combinations[3]}{(len(combinations[0]) - len(combinations[3])) * ' '} x {payout_rates[3]}
+        {combinations[4]}{(len(combinations[0]) - len(combinations[4])) * ' '} x {payout_rates[4]}
+        {combinations[5]}{(len(combinations[0]) - len(combinations[5])) * ' '} x {payout_rates[5]}
+        {combinations[6]}{(len(combinations[0]) - len(combinations[6])) * ' '} x {payout_rates[6]}
+        {combinations[7]}{(len(combinations[0]) - len(combinations[7])) * ' '} x {payout_rates[7]}
+        {combinations[8]}{(len(combinations[0]) - len(combinations[8])) * ' '} x {payout_rates[8]}''')
 
 print(f'''Добро пожаловать в игру "Видео покер". Задача игрока собрать как можно более сильную покерную комбинацию:
-        Чтобы выйграть необходимо собрать комбинацию от пары валетов или старше ("Jacks or better").
-        {payout_table}''')
+Чтобы выйграть необходимо собрать комбинацию от пары валетов или старше ("Jacks or better").
+{payout_table}
+Для замены карт используйте числа от 1 до 5 без пробелов, где число соответствует позиции карты слева направо.
+Например:
+Вам раздали A♣ K♥ T♣ T♥ 9♠
+Чтобы заменить A♣ K♥ 9♠ необходимо ввести 125
+Обратите внимание, что числа вводятся без пробелов, также порядок цифр не имеет значения:
+125 дает тот же результат, что и 152, 215, 251, 521 и 512.
+ ''')
+print('У Вас', points, 'очков!')
+
 while points > 0 and exit_request != 'y' and exit_request != 'у':
-    print(f'''Добро пожаловать в игру "Видео покер". Задача игрока собрать как можно более сильную покерную комбинацию:
-            Чтобы выйграть необходимо собрать комбинацию от пары валетов или старше ("Jacks or better").
-            {payout_table}''')
-    print('У Вас', points, 'очков!')
+    to_change_flag = False  # сбрасываем флаг проверки корректности выбранных к замене карт
     print('Введите ставку:')
-    bet = int(input())
+    bet = input()  # ставка игрока
+    try:  # проверка ставки игрока на корректность данных
+        bet = int(bet)
+    except ValueError:
+        print('Некорректная ставка. Введите действиетльное (целое) число ( > 0)')
+        continue
+
     points -= bet
     cards = cards_deck.copy()  # загружаем карты в игровую колоду. Имитация возврата карт в колоду после раунда
     random.shuffle(cards)  # Тасуем карты
 
     player_cards = [cards.pop(0) for _ in range(5)]  # Выдаем 5 карт игроку, удаляя выданное из игровой колоды
     print(*player_cards)  # Показываем карты игроку
-    print('Выберите карты для замены. Введите порядковые номера карт для замены (от 1 до 5)')
-    to_change = list(input())  # создание списка к замене
+
+    print('Выберите карты для замены. Введите порядковые номера карт без пробелов для замены (от 1 до 5)')
+
+    while to_change_flag is False:
+        to_change = list(input())  # создание списка к замене
+        count = 0
+        for changed in to_change:
+            if changed.isdigit() is False:
+                continue
+            else:
+                if int(changed) > 5:
+                    continue
+                else:
+                    count += 1
+        if count == len(to_change):
+            to_change_flag = True
+        else:
+            print('Для выбора карт используйте целые числа от 1 до 5\nВыберите карты для замены. ')
     to_change = [int(to_change[i]) for i in range(len(to_change))]
 
     for i in to_change:  # замена выбранных карт
@@ -132,7 +157,11 @@ while points > 0 and exit_request != 'y' and exit_request != 'у':
     else:
         print('К сожалению, Вы проиграли.')
     print(f'У Вас {points} очков!')
+    if points == 0:  # Какой смысл предлагать продолжать Играть если все проебано :(
+        break
     print('Если хотите продожить нажмите Enter. Для выхода введите "y"')
     exit_request = input()
-
-print(f'Поздравляем Ваш баланс {points} очков. \nСпасибо за игру')
+if points == 0:
+    print('Вы все проиграли, барин')
+else:
+    print(f'Поздравляем Ваш баланс {points} очков.\nСпасибо за игру')
